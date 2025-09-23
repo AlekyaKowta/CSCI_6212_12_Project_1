c9b0a491f3eece22618f04b9f0994e93aeebab54
AlekyaKowta <akowta@gmail.com>
2025-09-23 19:59:27 -0400

    Scaling Factor

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,20 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         // Selecting random values of n
-        int[] nValues = {10, 50, 100, 200, 400, 800, 1600};
+        int[] nValues = {2, 10, 50, 100, 200, 400, 800, 1600, 2400, 6000};
+
+        //Scaling the N so that the graph is more meaningful
+
+        int nNormalizedValue = 2400;
+        double normalizationTime = 399; //Experimental Time for n = 2400
+
+        // Assuming T(n) = c * n ^ 3
+        // Calculate c as Experimental Time / Theoretical Value
+        //
+        double scalingFactor = normalizationTime / Math.pow(nNormalizedValue, 3);
 
         // Printing the time in ms against the n values
-        System.out.println("n, time_ms, theoretical_time_ms");
+        System.out.println("n, time_ms, theoretical_time, adjusted_theoretical_time");
 
         for (int n : nValues) {
             long startTime = System.nanoTime();
@@ -16,8 +26,10 @@ public class Main {
             long endTime = System.nanoTime();
 
             long durationMs = (endTime - startTime) / 1000000;
-            long theoreticalDurationMs = (long) Math.pow(n, 3);
-            System.out.println(n + "," + durationMs + "," + theoreticalDurationMs);
+            long theoreticalDuration = (long) Math.pow(nNormalizedValue, 3);
+            double adjustedTheoreticalTime = scalingFactor * Math.pow(n, 3);
+
+            System.out.println(n + "," + durationMs + "," + theoreticalDuration + "," + adjustedTheoreticalTime);
         }
     }
 
